20b25a96e76ba5310b87d91d4a5ac0a554b66573
Mykola Stesiuk <mikalaj.krutak@gmail.com>
2025-05-15 18:48:56 +0200

    final code

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,10 +1,71 @@
 'use strict';
 
+const customer = {
+  money: 3000, // customer account balance
+  vehicle: {
+    maxTankCapacity: 40, // fuel tank volume
+    fuelRemains: 8, // Remaining fuel in the tank
+  },
+};
+
 describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+  const { fillTank } = require('./fillTank');
 
-  it('should ', () => {
+  it('should take a money from customer if arguments is valid', () => {
+    const testCustomer = JSON.parse(JSON.stringify(customer));
 
+    fillTank(testCustomer, 10, 10);
+
+    expect(testCustomer.money).toBe(2900);
+  });
+
+  it('should order full tank if amount is not given', () => {
+    const testCustomer = JSON.parse(JSON.stringify(customer));
+
+    fillTank(testCustomer, 10);
+
+    expect(testCustomer.vehicle.fuelRemains).toBe(40);
+  });
+
+  it('should order full tank if amount is greater', () => {
+    const testCustomer = JSON.parse(JSON.stringify(customer));
+
+    fillTank(testCustomer, 10, 100);
+
+    expect(testCustomer.vehicle.fuelRemains).toBe(40);
+  });
+
+  it('shoul order only amount that clien can to pay', () => {
+    const testCustomer = JSON.parse(JSON.stringify(customer));
+
+    fillTank(testCustomer, 1000, 10);
+
+    expect(testCustomer.vehicle.fuelRemains).toBe(11);
+  });
+
+  it('should round ordered sum', () => {
+    const testCustomer = JSON.parse(JSON.stringify(customer));
+
+    fillTank(testCustomer, 10, 5, 10, 5);
+
+    expect(testCustomer.vehicle).toEqual({
+      maxTankCapacity: 40,
+      fuelRemains: 13,
+    });
+  });
+
+  it('should not order customer if sum of order < 2L', () => {
+    const testCustomer = JSON.parse(JSON.stringify(customer));
+
+    fillTank(testCustomer, 10, 1);
+
+    expect(testCustomer).toEqual({
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    });
   });
 
   // write tests here
